2f4609039d8dd55b615aecbfd7a08de29d17c14f
devasol2008 <solankidevanshi80@gmail.com>
2025-09-24 10:03:01 +0530

    Add files via upload

---
+++ function4.js
@@ -0,0 +1,38 @@
+// let add = () => {
+//     let a = 2
+//     let b = 4
+//     console.log(a + b)
+// }
+// add()
+
+
+// with parameter function
+// let diff = (a, b) => {
+
+//     console.log(a - b)
+
+// }
+// diff(2, 3)
+
+// with multipal parmeters
+// let avg = (hindi, eng, maths, his, geo) => {
+//     let totle = (hindi + eng + maths + his + geo) / 5
+//     console.log(totle, "%")
+// }
+// avg(90, 80, 80, 90, 70)
+
+
+//  hello with arrow function
+// let print = () => console.log("hello")
+// print()
+
+// let print = (greet) => {
+
+//     console.log("hello Devanshi", greet)
+// }
+// print('morning')
+// print('afternoon')
+
+// // cube witharrow function
+// let cube = n => n * n * n;
+// console.log(cube(2))
