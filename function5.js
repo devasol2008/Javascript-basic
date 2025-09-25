717481d8f0d92c51b27041343e3fe21dca4c9ac7
devasol2008 <solankidevanshi80@gmail.com>
2025-09-25 10:07:20 +0530

    Add files via upload

---
+++ function5.js
@@ -0,0 +1,24 @@
+// // // anonymous
+
+// // let type = function () {
+// //     console.log("hello ,This is Devanshi");
+
+// // }
+// // type()
+
+// // setTimeout(function () {
+// //     console.log("Solanki Devanshi");
+// // }, 1000)
+
+// // main function
+// function muldiv(n1, n2, callback) {
+//     let product = n1 * n2;
+//     console.log(product)
+//     callback(n1, n2)
+// }
+// div function
+// function div(n1, n2) {
+//     console.log(n1 / n2)
+// }
+// calling
+// muldiv(2, 8, div)
